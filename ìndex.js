c9395c21cebcb24086c26f214172c85fb2ab38d9
zeeben59 <zeeben59@gmail.com>
2024-10-12 17:36:33 +0100

    Add files via upload

---
+++ ìndex.js
@@ -0,0 +1,22 @@
+// function toggleMenu() {
+//     const menu = document.querySelector(".menu-links");
+//     const icon = document.querySelector(".hamburger-icon");
+//     menu.classList.toggle("open");
+//     icon.classList.toggle("open");
+//   }
+
+
+// const hamburgerIcon = document.getElementById('hamburger-icon');
+// const menuLinks = document.getElementById('menu-links');
+
+// // Add event listener to toggle the display of the menu
+// hamburgerIcon.addEventListener('click', () => {
+//     if (menuLinks.style.display === 'flex') {
+//         menuLinks.style.display = 'none';  // Hide the menu
+//     } else {
+//         menuLinks.style.display = 'flex';  // Show the menu
+//     }
+// });
+
+  
+
